44c6aff8f7e39dc0cb80d0a554473107c9e48beb
ayden <abaayd01@gmail.com>
2023-03-18 16:15:40 +1030

    tidy up main.go error handling

--- main.go
+++ main.go
@@ -2,8 +2,87 @@ package main
 
 import (
 	"flag"
-	"log"
+	"fmt"
+	"os"
 )
+
+type exitCode int
+
+const (
+	exitOK    exitCode = 0
+	exitError exitCode = 1
+)
+
+func main() {
+	code := mainRun()
+	os.Exit(int(code))
+}
+
+func mainRun() exitCode {
+	config := parseArgs()
+
+	g, err := NewGenerator(config.inputFilePath, config.appName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to create generator: %s\n", err)
+		return exitError
+	}
+
+	w := NewWriter(config.outputDir)
+
+	routesFileBuf, err := g.GenerateRoutesFile()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to generate routes file: %s\n", err)
+		return exitError
+	}
+
+	err = w.WriteRoutesFile(routesFileBuf)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to write routes file: %s\n", err)
+		return exitError
+	}
+
+	actionFileBufs, err := g.GenerateActionFiles()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to generate action files: %s\n", err)
+		return exitError
+	}
+
+	err = w.WriteActionFiles(actionFileBufs)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to write action files: %s\n", err)
+		return exitError
+	}
+
+	serviceFileBufs, err := g.GenerateServiceFiles()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to generate service files: %s\n", err)
+		return exitError
+	}
+	err = w.WriteServiceFiles(serviceFileBufs)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to write service files: %s\n", err)
+		return exitError
+	}
+
+	contractsFileBuf, err := g.GenerateContractsFile()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to generate contracts file: %s\n", err)
+		return exitError
+	}
+	err = w.WriteContractsFile(contractsFileBuf)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to write contracts file: %s\n", err)
+		return exitError
+	}
+
+	schemasFile, err := g.GenerateSchemasFile()
+	if err != nil {
+		return exitError
+	}
+	err = w.WriteSchemasFile(schemasFile)
+
+	return exitOK
+}
 
 type args struct {
 	inputFilePath string
@@ -23,59 +102,4 @@ func parseArgs() args {
 		appName:       *appNamePtr,
 		outputDir:     *outputDirPtr,
 	}
-}
-
-func main() {
-	var err error
-	defer func() {
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}()
-
-	config := parseArgs()
-
-	g, err := NewGenerator(config.inputFilePath, config.appName)
-	if err != nil {
-		return
-	}
-
-	w := NewWriter(config.outputDir)
-
-	routesFileBuf, err := g.GenerateRoutesFile()
-	if err != nil {
-		return
-	}
-
-	err = w.WriteRoutesFile(routesFileBuf)
-	if err != nil {
-		return
-	}
-
-	actionFileBufs, err := g.GenerateActionFiles()
-	if err != nil {
-		return
-	}
-	err = w.WriteActionFiles(actionFileBufs)
-	if err != nil {
-		return
-	}
-
-	serviceFileBufs, err := g.GenerateServiceFiles()
-	if err != nil {
-		return
-	}
-	err = w.WriteServiceFiles(serviceFileBufs)
-
-	contractsFileBuf, err := g.GenerateContractsFile()
-	if err != nil {
-		return
-	}
-	err = w.WriteContractsFile(contractsFileBuf)
-
-	schemasFile, err := g.GenerateSchemasFile()
-	if err != nil {
-		return
-	}
-	err = w.WriteSchemasFile(schemasFile)
 }
