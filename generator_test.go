90ad268d4c3505caee687dec8289b318bfb94f40
ayden <abaayd01@gmail.com>
2023-03-18 16:41:07 +1030

    move toRackPath test into generator_test.go file

---
+++ generator_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_toRackPath(t *testing.T) {
+	type args struct {
+		codegenPath string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "it converts a path with a single param correctly.",
+			args: args{
+				codegenPath: "/users/{user_id}",
+			},
+			want: "/users/:user_id",
+		},
+		{
+			name: "doesn't do anything if there's no path params",
+			args: args{
+				codegenPath: "/users",
+			},
+			want: "/users",
+		},
+		{
+			name: "still works if there's multiple path params",
+			args: args{
+				codegenPath: "/goals/{goal_id}/key_result/{key_result_id}",
+			},
+			want: "/goals/:goal_id/key_result/:key_result_id",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := toRackPath(tt.args.codegenPath); got != tt.want {
+				t.Errorf("toRackPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
